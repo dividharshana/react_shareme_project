24aedbc9e426f11cc54591d3d1b803f78a0e5ab0
Dividharshana <dividharshana2021@gmail.com>
2023-02-13 16:04:43 +0530

    new commit

---
+++ shareme_backend/sanity.config.js
@@ -0,0 +1,24 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+
+export default defineConfig({
+  name: 'default',
+  title: 'shareme_jsm',
+
+  projectId: 'vkd55aiq',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
+
+
+
+
